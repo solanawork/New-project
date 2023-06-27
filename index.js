45fbbf54089333b7804b9e5c64910cf2c433016d
solanawork <businessprivat3@gmail.com>
2023-06-27 20:14:18 +0200

    set up firebase database

---
+++ index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+        databaseURL: "https://playground-57369-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const salesRepInDB = ref(database, "salesRep")
+
+
